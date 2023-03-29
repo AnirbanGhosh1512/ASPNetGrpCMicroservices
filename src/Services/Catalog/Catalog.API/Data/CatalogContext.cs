2b70b1c0573c144a264756168d2195203a62fb86
Anirban07 <anirban.ghosh1512@gmail.com>
2023-03-29 15:44:17 +0200

    CatalogDatabase Configuration and Entity Data Mapping Initials ready.

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+
+            //Fetch Database connection and database information from API Settings file. 
+            //Author: Anirban Ghosh
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            //This method is to see the collection after the database operation is done and this is singletone approach
+            //as it is call only when constructor of this class called. 
+            //Author: Anirban Ghosh
+            CatalogContextSeed.SeedData(Products);
+
+
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}

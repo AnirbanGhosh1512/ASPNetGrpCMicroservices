9ab9fc9095df8717afe43d08ae3d69e689f333d7
Anirban07 <anirban.ghosh1512@gmail.com>
2023-03-31 15:21:21 +0200

    Repository Structure Added. @Anirban 31.03.2023

---
+++ src/Services/Catalog/Catalog.API/Repository/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.API.Entities;
+
+namespace Catalog.API.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+
+        Task<Product> GetProductById(string id);
+
+        Task<IEnumerable<Product>> GetProductByName(string name);
+
+        Task<IEnumerable<Product>> GetProductByCatagory(string catagoryName);
+
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+    }
+}
